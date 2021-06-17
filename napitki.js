77c094db09dd16b6470159a83bcdd9e322f84b9a
Your Name <smirnova.d.d@gmail.com>
2021-06-17 14:42:35 +0300

    korzina

---
+++ napitki.js
@@ -0,0 +1,67 @@
+<!--ЗАКАЗ НАПИТКОВ-->
+<script>
+$( document ).ready(function() {
+$('[href = "#prodimg"]').addClass('prodimg');
+
+$('[href = "#barbariska"]').addClass('button_barbariska');
+$('.button_barbariska').attr('href' , '#order:Барбариска = 80:::image=https://static.tildacdn.com/tild3561-6138-4865-b833-633264346661/DSC_7211.JPG');
+
+$('[href = "#cherry"]').addClass('button_cherry');
+$('.button_cherry').attr('href' , '#order:Вишня = 80:::image=https://static.tildacdn.com/tild3035-6430-4435-b862-306661313438/DSC_7212.JPG');
+
+$('[href = "#pears"]').addClass('button_pears');
+$('.button_pears').attr('href' , '#order:Груши = 50:::image=https://static.tildacdn.com/tild6665-6266-4432-a331-653638313933/DSC_7220.JPG');
+
+$('[href = "#pears2"]').addClass('button_pears2');
+$('.button_pears2').attr('href' , '#order:Дюшес = 80:::image=https://static.tildacdn.com/tild6138-3864-4338-b834-383262323937/DSC_7241.JPG');
+
+$('[href = "#limonad"]').addClass('button_limonad');
+$('.button_limonad').attr('href' , '#order:Лимонад = 80:::image=https://static.tildacdn.com/tild6632-3734-4339-b931-316239313533/DSC_7215.JPG');
+
+$('[href = "#tarhun"]').addClass('button_tarhun');
+$('.button_tarhun').attr('href' , '#order:Тархун = 80:::image=https://static.tildacdn.com/tild3035-6436-4666-a339-343639643762/DSC_7237.JPG');
+
+$('[href = "#sernovod"]').addClass('button_sernovod');
+$('.button_sernovod').attr('href' , '#order:Серноводская = 80:::image=https://static.tildacdn.com/tild6137-6537-4261-a436-376230663761/DSC_7218.JPG');
+
+$('[href = "#sernovodyod"]').addClass('button_sernovodyod');
+$('.button_sernovodyod').attr('href' , '#order:Серноводская с фтором и йодом = 80:::image=https://static.tildacdn.com/tild3666-3362-4939-b761-626536356664/DSC_7239.JPG');
+
+$('[href = "#greenaiwa"]').addClass('button_greenaiwa');
+$('.button_greenaiwa').attr('href' , '#order:Лимонад Green Aiwa = 90:::image=https://static.tildacdn.com/tild3935-3538-4466-b263-646130656639/IMG_4014.png');
+
+$('[href = "#shaudan"]').addClass('button_shaudan');
+$('.button_shaudan').attr('href' , '#order:Лимонад Шау Дан = 90:::image=https://static.tildacdn.com/tild6536-3464-4863-b239-386235616462/IMG_4007.png');
+
+$('[href = "#tuse"]').addClass('button_tuse');
+$('.button_tuse').attr('href' , '#order:Лимонад ТуСэ = 90:::image=https://static.tildacdn.com/tild6439-6362-4664-a430-313539653033/IMG_4008.png');
+
+$('[href = "#blacksweet"]').addClass('button_blacksweet');
+$('.button_blacksweet').attr('href' , '#order:Лимонад Black Sweet = 90:::image=https://static.tildacdn.com/tild3761-3164-4561-b438-356262666266/IMG_4009.png');
+
+$('[href = "#mumbai"]').addClass('button_mumbai');
+$('.button_mumbai').attr('href' , '#order:Лимонад Мум Баи = 90:::image=https://static.tildacdn.com/tild3466-6639-4930-b033-323435633566/IMG_4010.png');
+
+$('[href = "#lavande"]').addClass('button_lavande');
+$('.button_lavande').attr('href' , '#order:Лимонад Lavande = 90:::image=https://static.tildacdn.com/tild6536-3464-4863-b239-386235616462/IMG_4012-removebg-pr.png');
+
+$('[href = "#jasmin"]').addClass('button_jasmin');
+$('.button_jasmin').attr('href' , '#order:Лимонад Jasmin = 90:::image=https://static.tildacdn.com/tild6465-6665-4632-b732-633536376136/IMG_4013.png');
+
+$('[href = "#limoncello"]').addClass('button_limoncello');
+$('.button_limoncello').attr('href' , '#order:Лимонад Limoncello = 90:::image=https://static.tildacdn.com/tild6165-3561-4839-a331-623564656434/IMG_4015.png');
+
+$('[href = "#blueagava"]').addClass('button_blueagava');
+$('.button_blueagava').attr('href' , '#order:Лимонад Blue Agava = 90:::image=https://static.tildacdn.com/tild6533-3461-4365-a439-346437363765/IMG_4016.png');
+
+$('[href = "#mint"]').addClass('button_mint');
+$('.button_mint').attr('href' , '#order:Лимонад Mint = 90:::image=https://static.tildacdn.com/tild3232-6338-4735-b539-346537616261/IMG_4011_1.png');
+
+
+
+$('.prodimg').not('.prodimg:first').hide();
+
+var number = 0;
+
+});    
+</script>
--- script.js
+++ script.js
@@ -6,6 +6,8 @@ $('document').ready(function(){
  
   loadGoods();
   loadNapitki();
+  //showMiniCart();
+  checkCart();
   showCart();
 });
 
@@ -15,7 +17,6 @@ function loadGoods() {
   $.getJSON('goods.json', function(data) {
     //console.log(data);
     let out = '';
-    goods = data;
     for (let key in data){
       
       out += '<div class="card">';
@@ -33,7 +34,7 @@ function loadGoods() {
       out += '</div>';
       out += '</div>';
       out += '<div class="price_wrp">';
-      out += '<p class="price">'+data[key]['cost']+'</p>';
+      out += '<p class="price">'+data[key]['cost']+' ₽</p>';
       out += '<button class="add-to-cart" data-art="'+key+'">Купить</button>';
       out += '</div>';
       out += '</div>';
@@ -41,7 +42,6 @@ function loadGoods() {
     }
     $('#goods').html(out);
     //$('button.add-to-cart').on('click', addToCart);
-    return(goods);
     
   });
 }
@@ -51,7 +51,6 @@ function loadNapitki() {
   $.getJSON('napitki.json', function(data) {
     //console.log(data);
     let out = '';
-    napitki = data;
     for (let key in data){
       
       out += '<div class="card">';
@@ -65,13 +64,14 @@ function loadNapitki() {
       out += '<div class="card_info">';
       out += '<div class="card_title">';
       out += '<h4>'+data[key]['name']+'</h4>';
+      out += '<p>'+data[key]['taste']+'</p>';
       out += '</div>';
       out += '<div class="description">';
       out += '<p>'+data[key]['description']+'</p>';
       out += '</div>';
       out += '</div>';
       out += '<div class="price_wrp">';
-      out += '<p class="price">'+data[key]['cost']+'</p>';
+      out += '<p class="price">'+data[key]['cost']+' ₽</p>';
       out += '<button class="add-to-cart" data-art="'+key+'">Купить</button>';
       out += '</div>';
       out += '</div>';
@@ -84,7 +84,7 @@ function loadNapitki() {
    
 
   })
-  return(napitki);
+  //return(napitki);
 }
 //console.log(napitki);
 
@@ -99,19 +99,19 @@ function addToCart() {
     cart[article] = 1;
   }
   localStorage.setItem('cart', JSON.stringify(cart) );
-  console.log(cart);
+  
   showCart();
 }
 
-function ckeckCart(){
+function checkCart(){
   //Проверяю наличие корзины в localStorage
   if ( localStorage.getItem('cart') != null) {
     cart = JSON.parse (localStorage.getItem('cart'));
   }
 }
 
-/*
-function showMiniCart(){
+
+/*function showMiniCart(){
   //показываю содержимое корзины
   let out = '';
   for (let i in cart) {
@@ -121,20 +121,85 @@ function showMiniCart(){
 }
 */
 
+
 function showCart() {
+  
+  $.when($.getJSON("goods.json"), $.getJSON("napitki.json")).done(function (data1, data2) {
+  
+  var tovary = Object.assign(data1[0], data2[0]);
+  //console.log(data1);
   var out = '';
-  for (var key in cart) {
-    out += '<button class="delete">x</button>';
-  }
+  
+  
+    out += '<div class="cart-title__wrp">'; //вывожу шапку корзины
+    out += '<div class="cart-icon">';
+    out += '<a href="#"><img src="img/icon_cart.svg"></a>';
+    out += '</div>';
+    out += '<div class="summa_zakaza">';
+    out += '<span>'+localStorage.getItem('total')+'</span>';
+    out += '</div>';
+    out += '</div>';
+    var total = 0;
+    localStorage.setItem('total', 0);
+           // total = JSON.parse (localStorage.getItem('total'));
+           // console.log(total);
+      for (let key in cart) { //вывожу содержимое корзины
+    
+        out += '<li class="cart-product cursorHover">';
+        out += '<div class="cart-product__row">';
+        out += '<div class="cart-product__name">'+tovary[key].name+' '+tovary[key].taste+'</div>';
+        out += '<div class="cart-product__quantity-container">';
+        out += '<div class="product-quantity-controls__container cart-product__quantity-slider">';
+        out += '<button type="button" class="product-quantity-controls__minus">-</button>';
+        out += '<span class="product-quantity-controls__value">'+cart[key]+'</span>';
+        out += '<button type="button" class="product-quantity-controls__plus">+</button>';
+        out += '</div>';
+        out += '</div>';
+        out += '</div>';
+        out += '<div class="cart-product__price-wrp">';
+        out += '<div class="cart-product__price">'+cart[key]*tovary[key].cost+'</div>';
+        out += '</div>';
+        out += '</li>';
+    
+  total += cart[key]*tovary[key].cost; //считаю общую сумму заказа
+  localStorage.setItem('total', parseInt(localStorage.getItem('total')) + cart[key]*tovary[key].cost); 
+    
+      }
+       /*if(localStorage.getItem('cart') != null) { //проверяю, есть ли товары в корзине
+    
+          if (localStorage.getItem('total') < 600) {//вывожу стоимость доставки, если сумма заказа меньше 600
+            let dostavka = 600 - localStorage.getItem('total');
+            out += '<div class="cart-delivery">';
+            out += '<div class="cart-delivery__content">';
+            out += '<span class="cart-delivery__title">Доставка</span>';
+            out += '<span class="cart-delivery__cost"> 99 ₽</span>';
+            out += '</div>';
+            out += '</div>';
+            out += '<div class="cart-alert">'; //вывожу остаток суммы, необходимой для бесплатной доставки
+            out += '<span class="cart-alert__text">Закажите еще на '+dostavka+' ₽ для бесплатной доставки</span>';
+            out += '</div>';
+          }
+        else if ((localStorage.getItem('total') >= 600)){
+          out += '<div class="cart-delivery">';
+          out += '<div class="cart-delivery__content">';
+          out += '<span class="cart-delivery__title">Доставка</span>';
+          out += '<span class="cart-delivery__cost"> 0 ₽</span>';
+          out += '</div>';
+          out += '</div>';
+        }
+      }*/
   $('#my-cart').html(out);
+  //console.log(data1[0]);
+  //console.log(data2[0]);
+});
+
 }
 
 showCart();
 
-$.when($.getJSON("goods.json"), $.getJSON("napitki.json")).done(function (
-  data1,
-  data2
-) {
-  console.log(data1[0]);
-  console.log(data2[0]);
-});
+/*
+function getTotal(total){
+  return(total);
+};
+getTotal(total);
+*/
